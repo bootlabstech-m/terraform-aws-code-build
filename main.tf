faf3e3d903482466883fa1beb1ef70bfa291743d
Silpashree <silpashree@bootlabstech.com>
2023-02-21 13:28:23 +0530

    new role ref

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ EOF
 resource "aws_codebuild_project" "storage_bucket"{
 name          = var.project_name
 build_timeout = var.timeout
-service_role  = aws_iam_role.s3_iam.arn
+service_role  = aws_iam_role.s3_iam_code_build.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
